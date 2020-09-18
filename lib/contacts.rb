828c33d390352d88344ee3f89d6f18e9a4c3813c
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 02:28:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash
+contacts.each do |person, contact_details_hash|
+  binding.pry
+  contact_details_hash.each do |favorite_ice_cream_flavors|
 end
